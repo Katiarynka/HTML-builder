f827733a111abccb01e9b38b2fbaa391705d1cdd
Katiarynka <katia.tsygankova@gmail.com>
2021-11-08 21:20:45 +0300

    feat: solve 01 and 02 tasks

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout, exit } = process;
+
+const newFileName = path.join(__dirname, 'test.txt');
+
+fs.writeFile(newFileName, '', error => { if (error) throw error; });
+
+stdout.write('Please, enter text:\n');
+stdin.on('data', data => {
+    const text = data.toString();
+    if (data.toString().trim() === 'exit') {
+        process.exit();
+    };
+    fs.appendFile(newFileName, data, error => { if (error) throw error; });
+});
+
+process.on('exit', () => {
+    stdout.write('Goodbye! Good luck!\n');
+});
